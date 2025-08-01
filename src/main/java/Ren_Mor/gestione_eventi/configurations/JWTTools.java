15ed0d095290c75b36f9987ec67f9a5384113545
Ren-Mor <renatomorelli@hotmail.it>
2025-08-01 13:04:04 +0200

    application properties e tools

---
+++ src/main/java/Ren_Mor/gestione_eventi/configurations/JWTTools.java
@@ -0,0 +1,40 @@
+package Ren_Mor.gestione_eventi.configurations;
+
+import Ren_Mor.gestione_eventi.entities.User;
+import Ren_Mor.gestione_eventi.exceptions.UnauthorizedException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JWTTools {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String createToken(User utente) {
+
+
+        return Jwts.builder()
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7))
+                .subject(String.valueOf(utente.getId()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+    }
+
+    public void verifyToken(String accessToken) {
+        try {
+            Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secret.getBytes())).build().parse(accessToken);
+        } catch (Exception ex) {
+
+            throw new UnauthorizedException("Problemi con il token! Effettuare di nuovo il login!"); // --> 401
+        }
+    }
+
+    public String extractIdFromToken(String accessToken) {
+        return Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secret.getBytes())).build().parseSignedClaims(accessToken).getPayload().getSubject();
+
+    }}
